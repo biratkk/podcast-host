7ba4119317527c03e99bf83e9a9166b313723599
biratkk <koiralabirat@gmail.com>
2022-02-04 03:56:07 +0000

    first commit - episode1 added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const episodeArr = [
+    {
+        name:"Episode 1 - Who am I?",
+        file:require('./Podcasts/Episode 1 - Who am I.mp3'),
+        episode:1
+    }
+];
+
+
+export default function App(){
+    return (
+        <div>
+            <h2>
+            All episodes
+            </h2>
+            {
+                episodeArr.map(episode => {
+                    return (
+                        <div className='episode-container'>
+                            <audio src={episode.file} controls={true} />
+                            {episode.name}
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
 }
-
-export default App;
